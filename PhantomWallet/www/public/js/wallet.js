3bfbfd4c06f73231144552170e52ea8d16b92e78
Vincent Geneste <vincentgeneste@gmail.com>
2019-07-30 16:26:58 -0700

    Revert "implemented all contract confirm box, started search feature on history page"
    This reverts commit 62e7d26a235bdc0dfc4356a40a3dcdd67c44a401.

--- PhantomWallet/www/public/js/wallet.js
+++ PhantomWallet/www/public/js/wallet.js
@@ -192,3 +192,14 @@ function toFixed(x) {
   }
   return x;
 }
+
+function imposeMinMax(el) {
+  if(el.value != ""){
+    if(parseInt(el.value) < parseInt(el.min)){
+      el.value = el.min;
+    }
+    if(parseInt(el.value) > parseInt(el.max)){
+      el.value = el.max;
+    }
+  }
+}
