c8baa15d04f89d0b243e138aed52b6708bc10f90
Vincent Geneste <vincentgeneste@gmail.com>
2019-09-29 02:23:11 -0700

    update package names

--- build_release.sh
+++ build_release.sh
@@ -12,46 +12,46 @@ if [ $platform  = "win" ]; then
     exit 1
 fi
 
-rm -rf bin/linux
-rm -rf bin/osx
-rm -rf bin/win
+rm -rf bin/PhantomWallet-linux
+rm -rf bin/PhantomWallet-osx
+rm -rf bin/PhantomWallet-win
 
 # create directories
-mkdir -p bin/linux
-mkdir -p bin/win
-mkdir -p bin/osx
+mkdir -p bin/PhantomWallet-linux
+mkdir -p bin/PhantomWallet-win
+mkdir -p bin/PhantomWallet-osx
 
-mkdir -p bin/linux/www
-mkdir -p bin/win/www
-mkdir -p bin/osx/www
+mkdir -p bin/PhantomWallet-linux/www
+mkdir -p bin/PhantomWallet-win/www
+mkdir -p bin/PhantomWallet-osx/www
 
 mkdir -p bin/release
 
 # package wallet
-third-party/warp-packer_$platform --arch linux-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/linux-x64/publish/ --exec PhantomWallet --output bin/linux/PhantomWallet
-third-party/warp-packer_$platform --arch macos-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/osx-x64/publish/ --exec PhantomWallet --output bin/osx/PhantomWallet
-third-party/warp-packer_$platform --arch windows-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/win-x64/publish/ --exec PhantomWallet.exe --output bin/win/PhantomWallet.exe
+third-party/warp-packer_$platform --arch linux-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/linux-x64/publish/ --exec PhantomWallet --output bin/PhantomWallet-linux/PhantomWallet
+third-party/warp-packer_$platform --arch macos-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/osx-x64/publish/ --exec PhantomWallet --output bin/PhantomWallet-osx/PhantomWallet
+third-party/warp-packer_$platform --arch windows-x64 --input_dir PhantomWallet/bin/Release/netcoreapp2.0/win-x64/publish/ --exec PhantomWallet.exe --output bin/PhantomWallet-win/PhantomWallet.exe
 
 # copy html/css stuff
-cp -r PhantomWallet/www/views  bin/linux/www
-cp -r PhantomWallet/www/views  bin/win/www
-cp -r PhantomWallet/www/views  bin/osx/www
+cp -r PhantomWallet/www/views  bin/PhantomWallet-linux/www
+cp -r PhantomWallet/www/views  bin/PhantomWallet-win/www
+cp -r PhantomWallet/www/views  bin/PhantomWallet-osx/www
 
-cp -r PhantomWallet/www/public bin/linux/www
-cp -r PhantomWallet/www/public bin/win/www
-cp -r PhantomWallet/www/public bin/osx/www
+cp -r PhantomWallet/www/public bin/PhantomWallet-linux/www
+cp -r PhantomWallet/www/public bin/PhantomWallet-win/www
+cp -r PhantomWallet/www/public bin/PhantomWallet-osx/www
 
-cp third-party/start.command bin/osx/
-cp third-party/start.sh bin/linux/
-cp third-party/start.bat bin/win/
+cp third-party/start.command bin/PhantomWallet-osx/
+cp third-party/start.sh bin/PhantomWallet-linux/
+cp third-party/start.bat bin/PhantomWallet-win/
 
 # package cli
-third-party/warp-packer_$platform --arch linux-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/linux-x64/publish/ --exec PhantomCli --output bin/linux/PhantomCli
-third-party/warp-packer_$platform --arch macos-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/osx-x64/publish/ --exec PhantomCli --output bin/osx/PhantomCli
-third-party/warp-packer_$platform --arch windows-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/win-x64/publish/ --exec PhantomCli.exe --output bin/win/PhantomCli.exe
+third-party/warp-packer_$platform --arch linux-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/linux-x64/publish/ --exec PhantomCli --output bin/PhantomWallet-linux/PhantomCli
+third-party/warp-packer_$platform --arch macos-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/osx-x64/publish/ --exec PhantomCli --output bin/PhantomWallet-osx/PhantomCli
+third-party/warp-packer_$platform --arch windows-x64 --input_dir PhantomCli/bin/Release/netcoreapp2.0/win-x64/publish/ --exec PhantomCli.exe --output bin/PhantomWallet-win/PhantomCli.exe
 
 # zip
-zip -r bin/release/linux.zip bin/linux
-zip -r bin/release/windows.zip bin/win
-zip -r bin/release/osx.zip bin/osx
+zip -r bin/release/PhantomWallet-linux.zip bin/PhantomWallet-linux
+zip -r bin/release/PhantomWallet-windows.zip bin/PhantomWallet-win
+zip -r bin/release/PhantomWallet-osx.zip bin/PhantomWallet-osx
 
