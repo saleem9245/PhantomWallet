1620ec6ee297a7f3ed03bf505938b0ab7f09d0ab
Vincent Geneste <vincentgeneste@gmail.com>
2019-10-11 20:45:03 -0700

    forced mainnet for everyone

--- PhantomWallet/ViewsRenderer.cs
+++ PhantomWallet/ViewsRenderer.cs
@@ -893,8 +893,8 @@ namespace Phantom.Wallet
         private static readonly Net[] Networks =
         {
             new Net{Name = "mainnet", IsEnabled = true, Value = 1},
-            new Net{Name = "testnet", IsEnabled = false, Value = 2},
-            new Net{Name = "simnet", IsEnabled = false, Value = 3},
+            //new Net{Name = "testnet", IsEnabled = false, Value = 2},
+            //new Net{Name = "simnet", IsEnabled = false, Value = 3},
         };
         #endregion
     }
